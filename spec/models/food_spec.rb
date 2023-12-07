637e58eae32711f5a8993fb5dd58efcbc6930981
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-07 16:26:55 -0300

    3° commit - tdd model: name consegue ser preenchido? GREEN

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Food, type: :model do
   describe 'Testes de preenchimento do model Food' do
 
     it 'name consegue ser preenchido?' do
-      @food.name = ''
+      @food.name = 'macarronada'
       expect(@food.name).to eq('macarronada')
     end
 
