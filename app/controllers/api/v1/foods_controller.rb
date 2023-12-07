e88ec71db8f8783770dca63bb4832d63eec4a010
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-07 17:10:21 -0300

    19° commit - tdd controller: Consegue excluir um food e retornar status 204? GREEN

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::FoodsController < ApplicationController
 
-  before_action :set_food, only: %i[show update] # show update destroy
+  before_action :set_food, only: %i[show update destroy] # show update destroy
 
   def index
     @foods = Food.all 
@@ -26,6 +26,10 @@ class Api::V1::FoodsController < ApplicationController
     else
       render json: @food.errors, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @food.destroy!
   end
 
 
