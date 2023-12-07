cf7c0e11037dd7b1f4e82fd310fd9f54def56d81
Adyson-Lima <adyson_lima@yahoo.com.br>
2023-12-07 16:53:18 -0300

    13° commit - tdd controller: Consegue listar um food especifico e retornar status 200? GREEN

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -1,10 +1,14 @@
 class Api::V1::FoodsController < ApplicationController
 
-  before_action :set_food, only: %i[] # show update destroy
+  before_action :set_food, only: %i[show] # show update destroy
 
   def index
     @foods = Food.all 
     render json: @foods
+  end
+
+  def show
+    render json: @food
   end
 
 private
